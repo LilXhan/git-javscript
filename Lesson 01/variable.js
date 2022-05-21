7de2a033f1aa7c2f5ccd9c6b2fe5e65ec23f19ae
Xhan <alvaradotuctod@gmail.com>
2022-05-21 08:06:39 -0500

    Variable 1
    varriable 1.1
    
    varriable 1.1

---
+++ Lesson 01/variable.js
@@ -0,0 +1,17 @@
+let cajaA = 3;
+cajaA = 5;
+let cajaB = 12;
+let cajaC = cajaA + cajaB;
+
+console.log(cajaA, cajaB, cajaC);
+
+//exercise
+
+let x = 2;
+let y = 3;
+let z = y;
+y = x;
+x = z;
+
+
+console.log('El resultado de la operacion es:', x, 'y', y);
